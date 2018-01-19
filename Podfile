0d136df9f6b32309863512b2695b19a3502776c3
Brijesh <brijesh2693@gmail.com>
2018-01-19 12:39:00 -0800

    Dev check in

--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 source 'https://github.com/CocoaPods/Specs.git'
+use_frameworks!
 target 'yaana' do
-pod 'GoogleMaps'
-pod 'GooglePlaces'
+pod 'Alamofire', '~> 4.5'
 end
---
+++ yaana/LoginRegister/LoginRegisterController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class LoginRegisterController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
