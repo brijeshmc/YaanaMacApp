4a8d61040449cf60be8b36900345edf1f2e49bfd
Brijesh <brijesh2693@gmail.com>
2018-02-07 11:56:22 -0800

    Dev changes

--- yaana/LoginRegister/LoginController.swift
+++ yaana/LoginRegister/LoginController.swift
@@ -3,6 +3,12 @@ import UIKit
 
 class LoginController : UIViewController {
 
+    let MOBILE_PATTERN : String = "[0-9]{10}";
+    
+    @IBOutlet weak var userNameErrorLabel: UILabel!
+    
+    @IBOutlet weak var passwordErrorLabel: UILabel!
+    
     @IBOutlet weak var mobileNumber: UITextField!
     
     @IBOutlet weak var password: UITextField!
@@ -10,7 +16,10 @@ class LoginController : UIViewController {
     //@IBOutlet weak var loginButtonText: UIButton!
     
     @IBAction func buttonClick(_ sender: Any) {
-        if (mobileNumber.text != "" && password.text != "") {
+        
+        let isUserNameValid : Bool = validateUserName(userName: mobileNumber.text!, invalidUserNameLabel: userNameErrorLabel)
+        let isPasswordValid : Bool = validatePassword(password: password.text!, invalidPasswordLabel: passwordErrorLabel)
+        if (isUserNameValid && isPasswordValid) {
             let configuration = URLSessionConfiguration .default
             let session = URLSession(configuration: configuration)
             
@@ -22,7 +31,10 @@ class LoginController : UIViewController {
             let userNameItem = URLQueryItem(name: "userName", value: mobileNumber.text)
             let passwordItem = URLQueryItem(name: "password", value: password.text)
             urlComponents.queryItems = [userNameItem,passwordItem]
-            guard let url = urlComponents.url else { fatalError("Could not create URL from components") }
+            guard let url = urlComponents.url else {
+                self.view.showToast(toastMessage: "Internal Server Error", duration: 3.0)
+                return
+            }
             
             var request = URLRequest(url: url)
             request.httpMethod = "POST"
@@ -36,7 +48,7 @@ class LoginController : UIViewController {
                 // 1: Check HTTP Response for successful GET request
                 guard let httpResponse = response as? HTTPURLResponse, let receivedData = data
                     else {
-                        print("error: not a valid http response")
+                        self.view.showToast(toastMessage: "Internal Server Error", duration: 3.0)
                         return
                 }
                 
@@ -45,22 +57,21 @@ class LoginController : UIViewController {
                 case 200:
                     
                     do {
-                        //Decode retrived data with JSONDecoder and assing type of Article object
-                        
                         let userLoginDomain = try JSONDecoder().decode(UserLoginDomain.self, from: receivedData)
                         
                         KeychainWrapper.standard.set(userLoginDomain.token, forKey:"yaana_token")
                         KeychainWrapper.standard.set(userLoginDomain.userDomain.userId, forKey:"yaana_user_id")
-                        
-                        print(KeychainWrapper.standard.string(forKey: "yaana_token") ?? "")
-                        print(KeychainWrapper.standard.long(forKey: "yaana_user_id") ?? 0)
+                        KeychainWrapper.standard.set(userLoginDomain.userDomain.displayName, forKey:"yaana_name")
+                        KeychainWrapper.standard.set(userLoginDomain.userDomain.email, forKey:"yaana_email")
+                        KeychainWrapper.standard.set(userLoginDomain.userDomain.mobileNo, forKey:"yaana_mobile_no")
+
                     } catch {
                         self.view.showToast(toastMessage: "Internal Server Error", duration: 3.0)
                         return
                     }
                     
                 default:
-                    print("save profile POST request got response \(httpResponse.statusCode)")
+                    self.view.showToast(toastMessage: "Internal Server Error", duration: 3.0)
                 }
             }
             dataTask.resume()
@@ -70,13 +81,37 @@ class LoginController : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //loginButtonText.layer.cornerRadius = 15;
-        //mobileNumber.becomeFirstResponder();
+        mobileNumber.becomeFirstResponder()
         
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func validateUserName(userName : String, invalidUserNameLabel : UILabel) -> Bool{
+        
+        if(userName.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).count == 0){
+            invalidUserNameLabel.text = "Mobile number is required"
+            invalidUserNameLabel.isHidden = false
+            return false
+        }
+        else{
+            
+        }
+        invalidUserNameLabel.isHidden = true
+        return true
+    }
+    
+    func validatePassword(password : String, invalidPasswordLabel : UILabel) -> Bool{
+        
+        if(password.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).count == 0){
+            invalidPasswordLabel.text = "Password is required"
+            invalidPasswordLabel.isHidden = false
+            return false
+        }
+        invalidPasswordLabel.isHidden = true
+        return true
     }
 }
