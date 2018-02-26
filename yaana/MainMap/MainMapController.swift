70ecb48a04498a606cbcf2c95676c8b92d2fcbc8
Brijesh <brijesh2693@gmail.com>
2018-02-26 12:28:58 -0800

    Dev changes

--- yaana/MainMap/MainMapController.swift
+++ yaana/MainMap/MainMapController.swift
@@ -1,28 +1,53 @@
 import UIKit
 import GoogleMaps
 
-class MainMapController: UIViewController {
+class MainMapController: UIViewController,CLLocationManagerDelegate  {
+    
+    @IBOutlet var googleMapsContainerView: UIView!
+    var resultsArray = [String]()
+    var googleMapsView:GMSMapView!
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Create a GMSCameraPosition that tells the map to display the
-        // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        view = mapView
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
         
-        // Creates a marker in the center of the map.
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
-        marker.map = mapView
+        self.googleMapsView = GMSMapView (frame: self.googleMapsContainerView.frame)
+        self.googleMapsView.settings.compassButton = true
+        self.googleMapsView.isMyLocationEnabled = true
+        self.googleMapsView.settings.myLocationButton = true
+        self.view.addSubview(self.googleMapsView)
 
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error)
+    {
+        print("Error" + error.localizedDescription)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        let userLocation = locations.last
+        let center = CLLocationCoordinate2D(latitude: userLocation!.coordinate.latitude, longitude: userLocation!.coordinate.longitude)
+        
+        let camera = GMSCameraPosition.camera(withLatitude: userLocation!.coordinate.latitude, longitude: userLocation!.coordinate.longitude, zoom: 15);
+        self.googleMapsView.camera = camera
+        self.googleMapsView.isMyLocationEnabled = true
+        
+        let marker = GMSMarker(position: center)
+
+        marker.map = self.googleMapsView
+        
+        marker.title = "Current Location"
+        locationManager.stopUpdatingLocation()
     }
 }
