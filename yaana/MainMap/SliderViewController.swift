c74fbe722c358a525f4e05ba5394c46b64dc1a54
Brijesh <brijesh2693@gmail.com>
2018-04-01 14:37:07 -0700

    Sliding navigation control changes

--- yaana/MainMap/SliderViewController.swift
+++ yaana/MainMap/SliderViewController.swift
@@ -6,4 +6,24 @@ class SliderViewController : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func PromotionsButton(_ sender: Any) {
+    }
+    
+    @IBAction func MyWalletButton(_ sender: Any) {
+    }
+    
+    @IBAction func MyRidesButton(_ sender: Any) {
+    }
+    
+    @IBAction func TermsAndConditionsButton(_ sender: Any) {
+    }
+    
+    @IBAction func AboutUsButton(_ sender: Any) {
+    }
+    
+    @IBAction func HelpCentreButton(_ sender: Any) {
+    }
+    @IBAction func SignOutButton(_ sender: Any) {
+    }
 }
